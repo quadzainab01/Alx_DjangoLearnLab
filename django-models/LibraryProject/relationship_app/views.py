c49e7200815484406e6462f36e9e59aaf7e3f2eb
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-07-21 00:35:04 +0100

    Add permission_required decorator import to views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required, user_passes_test
 from django.views.generic.detail import DetailView
+
 from .models import Book, Library
-from .forms import BookForm  # Ensure this exists or create one
+from .forms import BookForm  # Ensure this form exists
+
 
 # View: List all books
 def list_books(request):
