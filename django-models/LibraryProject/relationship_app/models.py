ae5ea90d329d6a2b824a078ee24f5f300427813f
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-07-20 20:35:44 +0100

    Fix Book model: add publication_year with default and run migrations

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,19 +6,20 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-
-    def __str__(self):
-        return self.title
-
 class Library(models.Model):
     name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField(default=2020)
+    libraries = models.ManyToManyField(Library, related_name='books')  # ✅ Define it here only
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
 
 class Librarian(models.Model):
     name = models.CharField(max_length=255)
