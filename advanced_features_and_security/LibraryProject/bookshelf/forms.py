464d43f686434326396561dad548a44ca2e90891
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-07-27 20:59:39 +0100

    Add CustomUser and ExampleForm, update Book model with permissions

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from .models import Book
 
-class CustomUserCreationForm(UserCreationForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'date_of_birth', 'profile_photo', 'password1', 'password2')
+        model = Book
+        fields = ['title', 'author', 'description']
