3df07c3ffcc0e7d51fc7acb1bf644c4a287eaba6
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-08-09 21:22:10 +0100

    Fix: Add missing permission imports IsAuthenticatedOrReadOnly and IsAuthenticated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics, permissions, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
