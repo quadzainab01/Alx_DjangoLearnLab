2e20fe02e46db1c477d73386d12195c81ca97baf
Zainab Quadri <quadz_d3a6qcz@GRATEFUL-HEART.S.A.W>
2025-08-09 21:24:38 +0100

    Add api.urls inclusion to project urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -20,5 +20,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('books/', include('books.urls')),
+    path('api-token-auth/', obtain_auth_token),
 ]
