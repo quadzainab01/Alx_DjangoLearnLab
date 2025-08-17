68351da1c9e95475f08b26134886894ba5a42b76
Your Name <you@example.com>
2025-08-17 11:26:30 +0100

    Add published_date field to Post model and apply migrations

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,13 +4,15 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.utils import timezone
-from .models import Post
+from django.urls import reverse_lazy
+
+from .models import Post, Profile
 from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
-# Home (latest 5 posts)
+# Home (latest 5 published posts)
 def home(request):
-    posts = Post.objects.order_by('-published_date')[:5]
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:5]
     return render(request, "blog/home.html", {"posts": posts})
 
 
@@ -29,12 +31,15 @@ def register(request):
     return render(request, "blog/register.html", {"form": form})
 
 
-# Profile Update
+# Profile Update (safe even if Profile doesn't yet exist)
 @login_required
 def profile(request):
+    # Ensure profile exists for this user
+    profile_obj, _created = Profile.objects.get_or_create(user=request.user)
+
     if request.method == "POST":
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile_obj)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -42,14 +47,13 @@ def profile(request):
             return redirect("blog:profile")
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+        p_form = ProfileUpdateForm(instance=profile_obj)
 
     return render(request, "blog/profile.html", {"u_form": u_form, "p_form": p_form})
 
 
 # --- Blog Post Views ---
 
-# List all posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
@@ -57,16 +61,15 @@ class PostListView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
-        return Post.objects.order_by('-published_date')
+        # Only show published posts publicly
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 
-# Show single post details
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
 
-# Create a new post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
@@ -74,16 +77,19 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        # Option A: publish immediately (uncomment next line)
+        form.instance.published_date = timezone.now()
+        # Option B: leave as draft (comment out the line above)
         return super().form_valid(form)
 
 
-# Update an existing post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
     template_name = 'blog/post_form.html'
 
     def form_valid(self, form):
+        # Keep author intact as current user
         form.instance.author = self.request.user
         return super().form_valid(form)
 
@@ -92,13 +98,39 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return self.request.user == post.author
 
 
-# Delete a post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    from django.urls import reverse_lazy
     success_url = reverse_lazy('blog:post-list')
 
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+
+# Optional: manual publish endpoint (useful if you allow drafts)
+@login_required
+def publish_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user == post.author:
+        post.publish()
+        messages.success(request, "Post published successfully.")
+    return redirect('blog:post-detail', pk=pk)
+
+
+print("publish_post in globals:", "publish_post" in globals())
+
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from .models import Post
+from django.utils import timezone
+
+@login_required
+def publish_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user == post.author:
+        post.publish()
+        messages.success(request, "Post published successfully.")
+    return redirect('blog:post-detail', pk=pk)
