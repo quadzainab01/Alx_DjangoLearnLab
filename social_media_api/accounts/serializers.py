212c3ba31b0d3e92d64b92f60c618c42e33c1e5b
Your Name <you@example.com>
2025-08-24 07:10:29 +0100

    Fix registration view: single RegisterView with token generation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,20 +1,23 @@
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user = CustomUser.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            bio=validated_data.get('bio', '')
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        Token.objects.create(user=user)  # create token for the user
         return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,15 +1,21 @@
 from rest_framework import generics
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .serializers import UserRegisterSerializer
-from .models import CustomUser
+from .serializers import RegisterSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserRegisterSerializer
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
 
     def create(self, request, *args, **kwargs):
+        # Use the serializer to create the user
         response = super().create(request, *args, **kwargs)
+
+        # Generate token for the created user
         token = Token.objects.get(user_id=response.data['id'])
         response.data['token'] = token.key
+
         return response
